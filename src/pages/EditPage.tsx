d47101d849c639287876d686ec97cd5dec90da7d
Sinji <sinzi031012@naver.com>
2024-06-08 17:34:40 +0900

    feat: 리스트, 생성, 수정 페이지 생성

---
+++ src/pages/EditPage.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function EditPage() {
+  return <EditPageWrapper>수정 페이지입니다~~</EditPageWrapper>;
+}
+
+export default EditPage;
+
+const EditPageWrapper = styled.section`
+  display: flex;
+
+  margin: 3rem;
+`;
---
+++ src/pages/ListPage.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function ListPage() {
+  return <ListPageWrapper>테스트입니다~~ </ListPageWrapper>;
+}
+
+export default ListPage;
+
+const ListPageWrapper = styled.section`
+  display: flex;
+
+  margin: 3rem;
+`;
