c620837dc792373987faa59f403066f245062faa
Sinji <sinzi031012@naver.com>
2024-06-12 03:19:59 +0900

    feat: 생성순, 수정순 정렬 구현

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -5,6 +5,7 @@ import Button from "../components/common/Button";
 import ListItem from "../components/ListItem";
 
 import { getSavedNoteInfo } from "../utils/getNoteInfo";
+import { handleUpdate, handleCreate } from "../utils/handle";
 
 import { ListItemProps } from "../types/listProps";
 
@@ -14,6 +15,27 @@ function ListPage() {
 
   return (
     <ListPageWrapper>
+      <BtnContainer>
+        <Dropdown
+          type="button"
+          onClick={() => {
+            handleCreate();
+            navigate("/");
+          }}
+        >
+          생성순
+        </Dropdown>
+        <Dropdown
+          type="button"
+          onClick={() => {
+            handleUpdate();
+            navigate("/");
+          }}
+        >
+          수정순
+        </Dropdown>
+      </BtnContainer>
+
       <ListContainer>
         {noteInfo.map((item: ListItemProps) => (
           <ListItem key={item.id} {...item} />
@@ -45,11 +67,31 @@ const ListContainer = styled.section`
   display: flex;
   flex-direction: column;
 
-  height: 32rem;
+  height: 30rem;
 
   overflow-y: scroll;
 
   &::-webkit-scrollbar {
     display: none;
   }
+`;
+
+const BtnContainer = styled.section`
+  display: flex;
+
+  margin-bottom: 1rem;
+`;
+
+const Dropdown = styled.button`
+  display: flex;
+  text-align: center;
+  justify-content: center;
+
+  width: 7rem;
+  height: 2rem;
+  margin-left: 1rem;
+
+  background-color: ${({ theme }) => theme.colors.white};
+
+  border-radius: 0.3rem;
 `;
--- src/utils/handle.tsx
+++ src/utils/handle.tsx
@@ -2,9 +2,8 @@ import moment from "moment";
 
 import { getSavedNoteInfo } from "../utils/getNoteInfo";
 
-const noteInfo = getSavedNoteInfo();
-
 export const handleEdit = (idx: number, title: string, body: string) => {
+  const noteInfo = getSavedNoteInfo();
   noteInfo[idx].title = title;
   noteInfo[idx].body = body;
   noteInfo[idx].updatedAt = moment().format("YYYY-MM-DD HH:mm:ss");
@@ -13,7 +12,26 @@ export const handleEdit = (idx: number, title: string, body: string) => {
 };
 
 export const handleDelete = (idx: number) => {
+  const noteInfo = getSavedNoteInfo();
   noteInfo.splice(idx, 1);
+  localStorage.setItem("noteInfo", JSON.stringify(noteInfo));
+};
+
+export const handleCreate = () => {
+  const noteInfo = getSavedNoteInfo();
+  noteInfo.sort((a, b) => {
+    return new Date(b.createdAt) - new Date(a.createdAt);
+  });
+
+  localStorage.setItem("noteInfo", JSON.stringify(noteInfo));
+};
+
+export const handleUpdate = () => {
+  const noteInfo = getSavedNoteInfo();
+  noteInfo.sort((a, b) => {
+    return new Date(b.updatedAt) - new Date(a.updatedAt);
+  });
+
   localStorage.setItem("noteInfo", JSON.stringify(noteInfo));
 };
 
